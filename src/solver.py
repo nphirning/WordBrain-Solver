e79ed19b5e409f57a218cbeba91a477c60f584de
nphirning <nphirning@gmail.com>
2017-01-03 13:20:07 -0800

    completed solver

--- src/solver.py
+++ src/solver.py
@@ -9,7 +9,7 @@ ADDED_WORDS = ['tv', 'haystack', 'crab', 'scooter', 'snowman', 'elk', 'chimney',
 BOARD_DIM = 4
 WORD_LENGTHS = [4,6,6]
 BOARD_STRING = 'raweebtlmturtins'
-ADVANCED = False
+ADVANCED = True
 
 ################# TRIE FUNCTIONS #################
 
@@ -38,6 +38,29 @@ def in_trie(trie, word):
             return 2 #Prefix is in trie
 
 ########################################################
+
+def generate_new_board(grid, word_data):
+    """ Given a board and a chosen word (with sequence of coords.)
+        generate a new board (simulating letters falling)
+
+    grid:       the current grid as a list of row-strings
+    word_data:  tuple of (word, array of coordinates for choosing the letters from grid)
+
+    Returns new board
+    """
+    new_board = []
+    temp = '?' * len(grid[0])
+    for i in range(len(grid)):
+        new_board.append(temp)
+
+    for col in range(len(grid[0])):
+        counter = len(grid)
+        for j in range(len(grid)):
+            row = len(grid) - j - 1
+            if (row, col) not in word_data[1] and grid[row][col] != '?':
+                counter -= 1
+                new_board[counter] = new_board[counter][:col] + grid[row][col] + new_board[counter][col+1:]
+    return new_board 
 
 def find_valid_words(grid, row, col, valid_lengths, word_list):
     """
@@ -90,6 +113,62 @@ def find_valid_words_helper(grid, row, col, current_string, explored_grid, valid
     explored_grid[row][col] = False
     return valid_words
 
+def grid_is_empty(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] != '?':
+                return False
+    return True
+
+def grid_is_full(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '?':
+                return False
+    return True
+
+def solve_wb(grid_arrays, valid_lengths, solutions, word_list):
+    # Solves wordbrain
+    for row in range(len(grid_arrays[-1][0])):
+        for col in range(len(grid_arrays[-1][0][0])):
+
+            remaining_valid_lengths = [valid_lengths[i][0] for i in range(len(valid_lengths)) if valid_lengths[i][1]]
+            valid_words = find_valid_words(grid_arrays[-1][0], row, col, remaining_valid_lengths, word_list)
+            """if grid_is_full(grid_arrays[-1][0]):
+                print "****************"
+                print row, col
+                print valid_words
+                print grid_arrays[-1][0]"""
+
+            for word_data in valid_words:
+                new_grid = generate_new_board(grid_arrays[-1][0], word_data)
+                grid_arrays.append([new_grid, word_data])
+                
+                """if grid_is_full(grid_arrays[-1][0]):
+                    print "LOOKING AT ", word_data
+                    print new_grid"""
+
+                for i in range(len(valid_lengths)):
+                    if valid_lengths[i][0] == len(word_data[0]) and valid_lengths[i][1] == True:
+                        valid_lengths[i][1] = False
+                        break
+                
+
+                if grid_is_empty(new_grid):
+                    solutions.append(copy.deepcopy(grid_arrays))
+                    print [x[1][0] for x in grid_arrays if x[1] != None]
+                else:
+                    solve_wb(grid_arrays, valid_lengths, solutions, word_list)
+
+                for i in range(len(valid_lengths)):
+                    if valid_lengths[i][0] == len(word_data[0]) and valid_lengths[i][1] == False:
+                        valid_lengths[i][1] = True
+                        break
+                
+
+
+                del grid_arrays[-1]
+    return solutions
 
 def main():
 
@@ -101,6 +180,22 @@ def main():
     word_dict = [word.lower() for word in f.read().split()]
     word_dict += ADDED_WORDS
     wst = make_trie(word_dict)
+
+    numbers = WORD_LENGTHS
+    boards = []
+    prev = 0
+    original_board = []
+    for i in range(BOARD_DIM, len(BOARD_STRING) + 1, BOARD_DIM):
+        original_board.append(BOARD_STRING[prev:i])
+        prev = i
+
+    solutions = []
+    boards.append([original_board, None])
+    valid_lengths_dict = []
+    for length in numbers:
+        valid_lengths_dict.append([length, True])
+
+    solve_wb(boards, valid_lengths_dict, solutions, wst)
 
     
 
