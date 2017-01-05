7d4d368f5283bd6902ab2794b68f780db8da6f08
nphirning <nphirning@gmail.com>
2017-01-05 01:56:00 -0800

    updated formatting; added license

--- test/test.py
+++ test/test.py
@@ -10,7 +10,12 @@ if __package__ is None:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from src import solver
 
-test_vals = [([4], 'oavl', 2, ['oval']), ([4,5], 'imclaraen', 3, ['nail', 'cream']), ([4,6,6], 'raweebtlmturtins', 4, ['turtle', 'timber', 'swan'])]
+test_vals = [\
+			([4], 'oavl', 2, ['oval']), \
+			([4,5], 'imclaraen', 3, ['nail', 'cream']), \
+			([4,6,6], 'raweebtlmturtins', 4, ['turtle', 'timber', 'swan'])\
+			### INSERT MORE TESTS HERE ###
+			]
 
 for test_tuple in test_vals:
 	if not test_tuple[3] in solver.main(ADVANCED, ADDED_WORDS, test_tuple[0], test_tuple[2], test_tuple[1], False):
